04db4a99447855c87f9a93120f619172eaa1b10f
gerrmanicuss <gerrmanicuss@gmail.com>
2020-06-21 21:00:03 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+alert('Hello, World!');
+
+let nameUser = prompt('What is you name?', 'user');
+alert(nameUser);
+
+let age = prompt('How old are you?', 0);
+alert(`You are ${age} old`);
+
+let isBoss = confirm('Are you a boss?');
+alert(isBoss);
+
+
