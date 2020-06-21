252b49d4b55517bd57bd315e43cb9289c719b99c
gerrmanicuss <gerrmanicuss@gmail.com>
2020-06-21 21:28:06 +0300

    third commit

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 alert('Hello, World!');
 
 let nameUser = prompt('What is you name?', 'user');
-alert(nameUser);
+alert(`Hi, ${nameUser}!`);
 
 let age = prompt('How old are you?', 0);
 alert(`You are ${age} years old`);
